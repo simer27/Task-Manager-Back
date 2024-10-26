bd898967c7d217eb4d3aa235233b9868afdc461c
Simeao Almeida <simer27@yahoo.com>
2024-10-26 18:55:42 -0300

    weatherforecast excluido

--- obj/Debug/net8.0/Task-Manager-Back.AssemblyInfo.cs
+++ obj/Debug/net8.0/Task-Manager-Back.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task-Manager-Back")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee5de4e132912508f4068bd51bfa1993d9544fb6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task-Manager-Back")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task-Manager-Back")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
